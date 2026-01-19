6a68e0f256ec8eed62f18db0be4d6201dfbf50be
nazitaOG <lopeznazarenoo@gmail.com>
2026-01-19 14:55:29 -0300

    main.ts implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser'; // <--- Importante
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser(process.env.COOKIE_SECRET));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
-bootstrap();
+void bootstrap();
